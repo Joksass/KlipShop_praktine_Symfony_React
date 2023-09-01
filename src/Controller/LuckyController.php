7faafdbc4aa8bd20fbdb769d4cf261b834d16124
Joksass <arnas.jok@gmail.com>
2023-09-01 10:26:08 +0300

    role hierarchy, home page update

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -13,16 +13,11 @@ class LuckyController extends AbstractController
     //#[Route('/lucky', name: 'app_lucky')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        $testRepository = $entityManager->getRepository(Test::Class);
-        $test = $testRepository->findAll();
-        $myname = $test[0]->getName(); //VIENA REIKIAMA PASIEMAM
+        // $testRepository = $entityManager->getRepository(Test::Class);
+        // $test = $testRepository->findAll();
+        // $myname = $test[0]->getName(); //VIENA REIKIAMA PASIEMAM
         
-        $number = random_int(0, 100);
-        return $this->render('lucky/index.html.twig', [
-            'controller_name' => 'LuckyController',
-            'lucky' => $number,
-            'test' =>$test,
-            'myname' => $myname,
-        ]);
+        // $number = random_int(0, 100);
+        return $this->render('lucky/index.html.twig');
     }
 }
